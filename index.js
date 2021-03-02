53ec15ec61a61112fde8ad538c741b5627f62332
diwei77chen <diwei77chen@gmail.com>
2021-03-02 15:48:48 +1100

    add back the other DB options than MemoryDb

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-'use strict';
-
-var MemoryDb = require('./lib/MemoryDb');
-module.exports = MemoryDb;
+exports.MemoryDb = require('./lib/MemoryDb');
+exports.LocalStorageDb = require('./lib/LocalStorageDb');
+exports.IndexedDb = require('./lib/IndexedDb');
+exports.WebSQLDb = require('./lib/WebSQLDb');
+exports.RemoteDb = require('./lib/RemoteDb');
+exports.HybridDb = require('./lib/HybridDb');
+exports.utils = require('./lib/utils');
